7aafc802019b834fc474de788d97a22fea085110
jh <jh@jan>
2024-11-28 09:30:14 +0100

    render pass

--- main.c
+++ main.c
@@ -53,6 +53,7 @@ struct {
   VkFormat swapChainImageFormat;
   VkExtent2D swapChainExtent;
   VkImageView* swapChainImageViews;
+  VkRenderPass renderPass;
   VkPipelineLayout pipelineLayout;
 } typedef App;
 
@@ -80,6 +81,8 @@ VkPresentModeKHR app_private_init_vulkan_create_swap_chain_choose_present_mode(V
 VkExtent2D app_private_init_vulkan_create_swap_chain_choose_swap_extend(VkSurfaceCapabilitiesKHR *capabilities, GLFWwindow *window);
 
 void app_private_init_vulkan_create_image_views(App *app);
+
+void app_private_init_vulkan_create_render_pass(App *app);
 
 void app_private_init_vulkan_create_graphics_pipeline(App* app);
 //------------------------------------
@@ -154,6 +157,7 @@ void app_private_init_vulkan(App* app) {
   app_private_init_vulkan_create_swap_chain(app);
   app_private_init_vulkan_create_image_views(app);
   app_private_init_vulkan_create_graphics_pipeline(app);
+  app_private_init_vulkan_create_render_pass(app);
 }
 
 void app_private_init_vulkan_create_instance(App* app) {
@@ -531,6 +535,46 @@ void app_private_init_vulkan_create_image_views(App* app) {
   }
 }
 
+void app_private_init_vulkan_create_render_pass(App *app) {
+  VkAttachmentDescription colorAttachment;
+  colorAttachment.format = app->swapChainImageFormat;
+  colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+  colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+  colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+  colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+  colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+  colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+  colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+  colorAttachment.flags = 0;
+
+  VkAttachmentReference colorAttachmentRef;
+  colorAttachmentRef.attachment = 0;
+  colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+  VkSubpassDescription subpass;
+  subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+  subpass.colorAttachmentCount = 1;
+  subpass.pColorAttachments = &colorAttachmentRef;
+  subpass.pDepthStencilAttachment = NULL;
+  subpass.preserveAttachmentCount = 0;
+  subpass.flags = 0;
+
+  VkRenderPassCreateInfo renderPassInfo;
+  renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+  renderPassInfo.attachmentCount = 1;
+  renderPassInfo.pAttachments = &colorAttachment;
+  renderPassInfo.subpassCount = 1;
+  renderPassInfo.pSubpasses = &subpass;
+  renderPassInfo.dependencyCount = 0;
+  renderPassInfo.pNext = NULL;
+  renderPassInfo.flags = 0;
+
+  if(vkCreateRenderPass(app->device, &renderPassInfo, NULL, &app->renderPass) != VK_SUCCESS) {
+    printf("failed to create render pass\n");
+    exit(1);
+  }
+}
+
 void app_private_init_vulkan_create_graphics_pipeline(App *app) {
   size_t vertCodeSize, fragCodeSize;
   uint8_t* vertShaderCode = helper_read_file("shaders/vert.spv", &vertCodeSize);
@@ -728,6 +772,7 @@ void app_private_main_loop(App* app) {
 
 void app_private_cleanup(App* app) {
   vkDestroyPipelineLayout(app->device, app->pipelineLayout, NULL);
+  vkDestroyRenderPass(app->device, app->renderPass, NULL);
 
   for(int i = 0; i < app->swapChainImagesCount; i++) {
     vkDestroyImageView(app->device, app->swapChainImageViews[i], NULL);
